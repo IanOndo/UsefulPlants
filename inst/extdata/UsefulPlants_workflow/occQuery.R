4d9b4484f27ecff53a15e6ef3f4ec9f5d5d294e3
Ian Ondo <I.Ondo@kew.org>
2019-12-11 19:40:43 +0000

    A couple of lines code edited

--- inst/extdata/UsefulPlants_workflow/occQuery.R
+++ inst/extdata/UsefulPlants_workflow/occQuery.R
@@ -41,15 +41,18 @@ cat('#====================================================================\n\n')
 run_name = RUN_NAME
 
 # get the list of species from file
-mySpecies_names = try(read.csv(SPECIES_NAME, header=TRUE)[,1],silent=TRUE)
+mySpecies_names = try(as.character(read.csv(SPECIES_NAME, header=TRUE)[,1]),silent=TRUE)
 
-if(!inherits(mySpecies_names,'error')){
+cat('> Species name(s):')
+if(!inherits(mySpecies_names,'try-error')){
   file.removed <- file.remove(SPECIES_NAME)
-  if(!file.removed)
-    warning(paste('Unable to remove species name file:', SPECIES_NAME))
+  if(!file.removed){
     cat('\n\n')
+    warning(paste('Unable to remove species name file:', SPECIES_NAME))
+  }
+  cat('...OK\n')
 }else{
-  stop('...Unable to read species names data...')
+  stop('...<< Unable to read species names data >>...')
 }
 if(length(mySpecies_names)==0L)
   stop("Oops !! Something probably went wrong when reading the species names...")
@@ -57,19 +60,27 @@ if(length(mySpecies_names)==0L)
 # get the list of data sources to use
 myData_sources  = unlist(stringr::str_split(DATA_SOURCE, pattern=","))
 
+cat('> Data source(s):')
+if(length(myData_sources)!=0L && sum(nchar(myData_sources))>0L){
+  cat('...OK\n')
+}else{
+  stop('...<< Please provide at least one valid data source >>...')
+}
+
 # ensure that the directories provided exists
-if(!dir.exists(DOWNLOAD_DIR)) dir.create(DOWNLOAD_DIR, recursive=TRUE)
+if(exists('DOWNLOAD_DIR'))
+  if(!dir.exists(DOWNLOAD_DIR)) dir.create(DOWNLOAD_DIR, recursive=TRUE)
 if(!dir.exists(OUTPUT_DIR)) dir.create(OUTPUT_DIR, recursive=TRUE)
 
 # Is there any offline databases required ?
-myOfflineData_sources <- intersect(myData_sources, c('biotime','rainbio', 'genesys'))
+myOfflineData_sources <- intersect(myData_sources, c('biotime','rainbio', 'genesys', 'genesys','spLink','cwr_gbif'))
 if(length(myOfflineData_sources)>0L){
   # Should some data sources be preformatted ?
   need_formatting = sapply(myOfflineData_sources, function(src) file.exists(system.file("extdata/Occ_dir/data_formatted",paste0(src,"DATA.rds"), package='UsefulPlants')))
 }else{
   need_formatting = FALSE
 }
-doFormatting = any(is_formatted)
+doFormatting = any(need_formatting)
 
 cat('\n\n')
 
@@ -78,7 +89,7 @@ cat('#== 2. Prepare databases\n')
 cat('#====================================================================\n')
 if(doFormatting){
   cat('...Formatting required databases...')
-  commandArgs <- function(...) paste0(intersect(myData_sources[!is_formatted], c('biotime','rainbio', 'genesys')))
+  commandArgs <- function(...) paste0(intersect(myData_sources[!need_formatting], c('biotime','rainbio', 'genesys','spLink','cwr_gbif')))
   assign('commandArgs',commandArgs,envir=.GlobalEnv)
   source(system.file("extdata/UsefulPlants_workflow/dbFormat.r",package='UsefulPlants'))
 }else{
@@ -91,9 +102,6 @@ cat('\n\n')
 cat('#====================================================================\n')
 cat('#== 3. Pre-schedule database query\n')
 cat('#====================================================================\n\n')
-
-cat('\n\n')
-
 if(length(myOfflineData_sources)>0L){
 
   cat('#--------------------------------------------------------------------\n')
@@ -130,7 +138,7 @@ cat('\n\n')
 cat('#====================================================================\n')
 cat('#== 4. Run queries\n')
 cat('#====================================================================\n')
-if(any(c('gbif','bien') %in% myData_sources)){
+if('gbif' %in% myData_sources){
   #Creating a GBIF login
   myGBIFLogin <- occCite::GBIFLoginManager(user = "iondo",
                                 email = "i.ondo@kew.org",
@@ -140,15 +148,16 @@ if(any(c('gbif','bien') %in% myData_sources)){
     Sys.setenv(GBIF_USER=myGBIFLogin@'username', GBIF_PWD=myGBIFLogin@'pwd', GBIF_EMAIL=myGBIFLogin@'email')
 }
 
-
 cat('...Starting extraction...\n\n')
 
 started.at <- Sys.time()
 mclapply(c(1:length(mySpecies_names)), function(j){
 
   # get database to query
-  offline_db  <- colnames(mydatabaseQueries)[mydatabaseQueries[j,]]
-  online_db   <- intersect(myData_sources, myOfflineData_sources)
+  offline_db  <- NULL
+  if(exists('mydatabaseQueries'))
+    offline_db  <- colnames(mydatabaseQueries)[mydatabaseQueries[j,]]
+  online_db   <- setdiff(myData_sources, myOfflineData_sources)
   db_to_query <- c(offline_db, online_db)
 
   # set gbif user account credentials
@@ -161,7 +170,10 @@ mclapply(c(1:length(mySpecies_names)), function(j){
     out = switch(db,
 
                  'gbif' = {
-                   gbifData <- UsefulPlants::queryGBIF(species_name = mySpecies_names[j], gbif_download_dir = file.path(DOWNLOAD_DIR, mySpecies_names[j]))
+                   if(!dir.exists(file.path(DOWNLOAD_DIR, mySpecies_names[j])))
+                     dir.create(file.path(DOWNLOAD_DIR, mySpecies_names[j]),recursive=TRUE)
+
+                   gbifData <- queryGBIF(species_name = mySpecies_names[j], gbif_login=myGBIFLogin, gbif_download_dir = file.path(DOWNLOAD_DIR, mySpecies_names[j]))
 
                    if(!is.null(gbifData)){
 
@@ -177,7 +189,7 @@ mclapply(c(1:length(mySpecies_names)), function(j){
                  },
 
                  'bien' = {
-                   bienData <- UsefulPlants::queryBIEN(species_name = mySpecies_names[j])
+                   bienData <- queryBIEN(species_name = mySpecies_names[j])
 
                    if(!is.null(bienData)){
 
@@ -238,7 +250,7 @@ mclapply(c(1:length(mySpecies_names)), function(j){
                  }
 
     )
-    if(class(out)=='try-error') cat(out)
+    if(inherits(out,'try-error')) cat(out)
     gc()
   }
 },mc.cores=MC_CORES, mc.preschedule = FALSE)
@@ -247,6 +259,6 @@ time.elapsed <- finished.at - started.at
 
 cat('...End of extraction...\n\n')
 cat(paste0('> End of Run ',"'",run_name,"'",' on: ',Sys.time()),'\n\n');finished.at=proc.time()
-cat(paste0('> Running time: ',as.numeric(time.elapsed), attr(time.elapsed,'units'),'\n'))
+cat(paste0('> Running time: ',as.numeric(time.elapsed),'', attr(time.elapsed,'units'),'\n'))
 cat(paste0('#',paste(rep('-',times=100),collapse="")))
 cat('\n\n')
